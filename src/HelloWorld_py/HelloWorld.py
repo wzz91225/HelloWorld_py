263549dd7f5864efce125c6903c68514b6e1dd5c
wzz91225 <wzz91225@outlook.com>
2019-09-25 01:23:28 +0800

    testing "for else"

--- src/HelloWorld_py/HelloWorld.py
+++ src/HelloWorld_py/HelloWorld.py
@@ -2,5 +2,12 @@
 # -*- coding: utf-8 -*-
 print("HelloWorld python")
 print("中文")
+
 for i in range(2, 5):
-    print(i)
+    print('i = ' + str(i))
+    for j in range(i, 4):
+        print('j = ' + str(j))
+    else:
+        print('else i = ' + str(i))
+else:
+    print('else finish')
