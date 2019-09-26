84f307fa444c51d271eb8f6ccf9df0648cd3555c
wzz91225 <wzz91225@outlook.com>
2019-09-26 09:40:53 +0800

    create binary tree

--- src/Algorithms/sort/HeapSort.py
+++ src/Algorithms/sort/HeapSort.py
@@ -3,7 +3,77 @@
 
 
 
-def HeapSort():
+class BinaryTreeNode():
+    def __init__(self, value = 0, left = None, right = None):
+        self.value = value
+        self.left = left
+        self.right = right
+    
+    def getValue(self):
+        return self.value
+    
+    def getLeft(self):
+        return self.left
+    
+    def getRight(self):
+        return self.right
+
+
+
+class BinaryTree():
+    def __init__(self, root_value = None):
+        if root_value == None:
+            self.root = None
+        else:
+            self.root = BinaryTreeNode(root_value)
+        self.current = self.root
+    
+    def getValue(self):
+        return self.current.getValue()
+    
+    def getLeft(self):
+        return self.current.getLeft()
+    
+    def getRight(self):
+        return self.current.getRight()
+    
+    def goLeft(self):
+        if self.current.left == None:
+            pass
+        else:
+            self.current = self.current.left
+    
+    def goRight(self):
+        if self.current.right == None:
+            pass
+        else:
+            self.current = self.current.Right
+    
+    def addLeft(self, left_value):
+        if self.current.left != None:
+            pass
+        else:
+            self.current.left = BinaryTreeNode(left_value)
+    
+    def addRight(self, right_value):
+        if self.current.right != None:
+            pass
+        else:
+            self.current.right = BinaryTreeNode(right_value)
+    
+    def preorderTraversal(self, node):
+        if node == None:
+            return
+        yield node
+        self.preorderTraversal(node.left)
+        self.preorderTraversal(node.right)
+    
+    def initTree_layer(self, f):
+        pass
+
+
+
+def HeapSort(f):
     pass
 
 
@@ -31,3 +101,9 @@ def OutputArr(f):
     except IOError:
         for i in range(len(f)):
             print(f[i])
+
+
+
+f = InputArr()
+HeapSort(f)
+OutputArr(f)
