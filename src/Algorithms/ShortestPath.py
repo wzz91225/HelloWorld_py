c661ca5699d9801381b874f39f07883a2b4ed123
wzz91225 <wzz91225@outlook.com>
2019-12-11 19:27:31 +0800

    Create ShortestPath.py

---
+++ src/Algorithms/ShortestPath.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+
+
+def Floyd():
+    pass
+
+
+
+def Dijkstra():
+    pass
+
+
+
+def Bellman_Ford():
+    pass
+
+
+
+def SPFA():
+    pass
+
+
+
+if __name__ == "__main__":
+    pass
